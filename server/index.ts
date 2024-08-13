ed93ffa512b83b977d24f4d8b1ba40f30c397aa3
Ahmet ATAR <128663943+CAPELLAX02@users.noreply.github.com>
2024-08-13 20:20:49 +0300

    sample data added

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,19 @@
 require('dotenv').config();
-import express from 'express';
+import express, { Express, Request, Response, NextFunction } from 'express';
 import { graphqlHTTP } from 'express-graphql';
+import schema from './schema/schema';
 
 const port = process.env.PORT || 5000;
 
-const app = express();
+const app: Express = express();
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === 'development',
+  })
+);
 
 app.listen(port, () => {
   console.log(`Server running in ${process.env.NODE_ENV} on port ${port}`);

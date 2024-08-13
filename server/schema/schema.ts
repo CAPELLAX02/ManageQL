1900b792302286776d51d8dcb1e2f5e8cf17f512
Ahmet ATAR <128663943+CAPELLAX02@users.noreply.github.com>
2024-08-13 20:36:32 +0300

    GraphQL schema initialized

--- server/schema/schema.ts
+++ server/schema/schema.ts
@@ -1,77 +1,41 @@
-const projects = [
-  {
-    id: '1',
-    clientId: '1',
-    name: 'eCommerce',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.',
-    status: 'In Progress',
-  },
-  {
-    id: '2',
-    clientId: '2',
-    name: 'Dating App',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.',
-    status: 'Done',
-  },
-  {
-    id: '3',
-    clientId: '3',
-    name: 'SEO Project',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.',
-    status: 'In Progress',
-  },
-  {
-    id: '4',
-    clientId: '4',
-    name: 'Design Prototype',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.',
-    status: 'Done',
-  },
-  {
-    id: '5',
-    clientId: '5',
-    name: 'Auction Website',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.',
-    status: 'In Progress',
-  },
-];
+import { projects, clients } from '../sampleData';
+import {
+  GraphQLObjectType,
+  GraphQLID,
+  GraphQLString,
+  GraphQLSchema,
+} from 'graphql';
 
-const clients = [
-  {
-    id: '1',
-    name: 'Tony Start',
-    email: 'ironman@gmail.com',
-    phone: '343-567-4333',
-  },
-  {
-    id: '2',
-    name: 'Natasha Romanova',
-    email: 'blackwidow@gmail.com',
-    phone: '223-567-3322',
-  },
-  {
-    id: '3',
-    name: 'Thor Odinson',
-    email: 'thor@gmail.com',
-    phone: '324-331-4333',
-  },
-  {
-    id: '4',
-    name: 'Steve Rogers',
-    email: 'captainamerica@gmail.com',
-    phone: '344-562-6787',
-  },
-  {
-    id: '5',
-    name: 'Bruce Banner',
-    email: 'hulk@gmail.com',
-    phone: '321-468-8887',
-  },
-];
+/**
+ * Client Type
+ */
+const ClientType = new GraphQLObjectType({
+  name: 'Client',
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    email: { type: GraphQLString },
+    phone: { type: GraphQLString },
+  }),
+});
 
-export { projects, clients };
+/**
+ * Root Query
+ */
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    client: {
+      type: ClientType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return clients.find((client) => client.id === args.id);
+      },
+    },
+  },
+});
+
+/**
+ * Export the root GraphQL query
+ */
+export default new GraphQLSchema({ query: RootQuery });

0873567814426586064b95ca9f0035731579f9e2
Ahmet ATAR <128663943+CAPELLAX02@users.noreply.github.com>
2024-08-14 04:25:52 +0300

    Apollo client mutations

--- client/src/components/ClientRow.tsx
+++ client/src/components/ClientRow.tsx
@@ -1,4 +1,7 @@
 import { FaTrash } from 'react-icons/fa';
+import { useMutation } from '@apollo/client';
+import { DELETE_CLIENT } from './mutations/clientMutations.ts';
+import { useEffect } from 'react';
 
 export interface IClient {
     id: string;
@@ -8,13 +11,28 @@ export interface IClient {
 }
 
 export default function ClientRow({ client }: { client: IClient }) {
+    const [deleteClient] = useMutation(DELETE_CLIENT, {
+        variables: { id: client.id },
+    });
+
+    const deleteHandler = () => {
+        if (window.confirm('Are you sure you want to delete this client?')) {
+            deleteClient();
+        }
+    };
+
+    useEffect(() => {}, [client]);
+
     return (
         <tr>
             <td>{client.name}</td>
             <td>{client.email}</td>
             <td>{client.phone}</td>
             <td>
-                <button className='btn btn-danger btn-sm'>
+                <button
+                    className='btn btn-danger btn-sm'
+                    onClick={deleteHandler}
+                >
                     <FaTrash />
                 </button>
             </td>
